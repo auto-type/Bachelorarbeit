ec6910ee166b2f726e9e76533ee922e536494575
Felix <bs29ebun@studserv.uni-leipzig.de>
2026-01-18 17:17:02 +0100

    ESP32 WIfi, touch and deepsleep capabilities implemented

--- Code/ESP32/Blinky/BLinky/BLinky.ino
+++ Code/ESP32/Blinky/BLinky/BLinky.ino
@@ -1,69 +1,31 @@
-#define LED 3
+/* 
+ 
+#define LED 2
+#define TOUCH 4
 
 void setup() {
-  
-  pinMode(LED,  OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);
-  pinMode(14, OUTPUT);
-  pinMode(15, OUTPUT);
-  pinMode(16, OUTPUT);
-  pinMode(17, OUTPUT);
-  pinMode(18, OUTPUT);
-  pinMode(19, OUTPUT);
 
+  Serial.begin(115200);
+  pinMode(LED,  OUTPUT);
+  delay(1000); // give me time to bring up serial monitor
+
+  Serial.println("Setting up for Touch sensitivity:\n");
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
-  digitalWrite(1, HIGH);
-  digitalWrite(3, HIGH);
-  digitalWrite(4, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, HIGH);
-  digitalWrite(9, HIGH);
-  digitalWrite(10, HIGH);
-  digitalWrite(11, HIGH);
-  digitalWrite(12, HIGH);
-  digitalWrite(13, HIGH);
-  digitalWrite(14, HIGH);
-  digitalWrite(15, HIGH);
-  digitalWrite(16, HIGH);
-  digitalWrite(17, HIGH);
-  digitalWrite(18, HIGH);
-  digitalWrite(19, HIGH);
-
+  
   delay(1000);
-  digitalWrite(LED, LOW);
-  digitalWrite(1, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(11, LOW);
-  digitalWrite(12, LOW);
-  digitalWrite(13, LOW);
-  digitalWrite(14, LOW);
-  digitalWrite(15, LOW);
-  digitalWrite(16, LOW);
-  digitalWrite(17, LOW);
-  digitalWrite(18, LOW);
-  digitalWrite(19, LOW);
-  delay(1000);  
+  if(touchRead(TOUCH) < 15){
+    digitalWrite(LED, HIGH);
+    }
+  else{
+    digitalWrite(LED, LOW);
+    }
+
+  Serial.print("Touch sensor: ");
+  Serial.println(touchRead(TOUCH));
+
+  
 }
+
+*/
